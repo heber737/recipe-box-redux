2194d726f772ab6d8b7a857a3f63d1ab03b81ccb
Heber Villalobos <hebervillalobos@gmail.com>
2024-11-21 03:24:53 -0500

    fix imports and update file tree

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import currentRecipeReducer from "../features/current-recipe/currentRecipeSlice.js";
+import modalTypeReducer from "../features/modal-type/modalTypeSlice.js";
+import formInputSlice from "../features/form-input/formInputSlice.js";
+import recipesSlice from "../features/recipes/recipesSlice.js";
+
+export const store = configureStore({
+  reducer: {
+    currentRecipe: currentRecipeReducer,
+    modalType: modalTypeReducer,
+    formInput: formInputSlice,
+    recipes: recipesSlice,
+  },
+});
